17bebf0a4a70d7cccd6e13f47894824f64f8cd47
dmitrydenisov183 <dmitrydenisov183@gmail.com>
2021-06-05 00:53:17 +1000

    adding what is eve page

---
+++ client/components/Navbar.tsx
@@ -0,0 +1,64 @@
+import { FC } from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { Navbar } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import styled from 'styled-components'
+
+const StyledNavbar = styled(Navbar)`
+    && {
+        background-color: #050610;
+        height: 57px;
+        border: 1px solid #333;
+        & > div {
+            width: 100%;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+        & button {
+            background-color: inherit;
+            border: none;
+        }
+        & svg {
+            width: 24px;
+            height: 24px;
+            color: #fff;
+        }
+    }
+`
+
+const StyledLogo = styled.div`
+    cursor: pointer;
+    margin-top: 6px;
+`
+
+const AppNavbar: FC = () => {
+    return (
+        <StyledNavbar>
+            <div>
+                <button>
+                    <FontAwesomeIcon icon={faChevronLeft} />
+                </button>
+
+                <StyledLogo>
+                    <Link href="/">
+                        <Image
+                            src="/eve.svg"
+                            alt="EVE Logo"
+                            width={72}
+                            height={28}
+                        />
+                    </Link>
+                </StyledLogo>
+
+                <button>
+                    <FontAwesomeIcon icon={faBars} />
+                </button>
+            </div>
+        </StyledNavbar>
+    )
+}
+
+export default AppNavbar
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,20 +1,46 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.css'
 import type {
-  ExternalProvider,
-  JsonRpcFetchFunc,
-} from "@ethersproject/providers";
-import { Web3Provider } from "@ethersproject/providers";
-import { Web3ReactProvider } from "@web3-react/core";
-import type { AppProps } from "next/app";
+    ExternalProvider,
+    JsonRpcFetchFunc,
+} from '@ethersproject/providers'
+import { Web3Provider } from '@ethersproject/providers'
+import { Web3ReactProvider } from '@web3-react/core'
+import type { AppProps } from 'next/app'
 
 function getLibrary(provider: ExternalProvider | JsonRpcFetchFunc) {
-  return new Web3Provider(provider);
+    return new Web3Provider(provider)
 }
 
 export default function EveApp({ Component, pageProps }: AppProps) {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  );
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Roboto:wght@100;300;400;500;700;900&display=swap"
+                    rel="stylesheet"
+                />
+                <link
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                    rel="stylesheet"
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                    crossOrigin="anonymous"
+                />
+                <script
+                    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+                    integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+                    crossOrigin="anonymous"
+                ></script>
+            </Head>
+            <Web3ReactProvider getLibrary={getLibrary}>
+                <Component {...pageProps} />
+            </Web3ReactProvider>
+        </>
+    )
 }
